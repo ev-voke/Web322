777b3547cb915932bf3a97ce2a0dbcbb98f47bbf
Jaz Sandhu <jaz.sandhu119@gmail.com>
2018-02-08 09:18:06 -0500

    Update server.js

--- Assignment 1/server.js
+++ Assignment 1/server.js
@@ -4,7 +4,7 @@ var app = express();
 
 //setup a 'route' to listen on the default url path
 app.get("/", (req,res) => {
-    res.send("Jasdeep Sandhu - 121190151");
+    res.send("Jaz");
 });
 
 //setup http server to listen on HTTP_PORT
